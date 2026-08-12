a4dff08cba94880eaf69630808f3e9c7853f7242
sklit-dsk <danilasklovskii@gmail.com>
2026-08-12 16:30:32 +0200

    Добавление новых URL-адресов для тестов и улучшение фикстур для генерации пользовательских данных

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -2,11 +2,14 @@ import pytest
 from playwright.sync_api import sync_playwright
 from constants.ui_constants import UIConstants
 from tools.trace import Tools
-
+from models.ui_models import UIUserDemoQA, UIUserCinescope
+from utils.data_generator import DataGenerator
 
 @pytest.fixture(scope="session")  # Браузер запускается один раз для всей сессии
 def browser(playwright):
-    browser = playwright.chromium.launch(headless=True)  # headless=True для CI/CD, headless=False для локальной разработки
+    browser = playwright.chromium.launch(
+        headless=True
+    )  # headless=True для CI/CD, headless=False для локальной разработки
     yield browser  # yield возвращает значение фикстуры, выполнение теста продолжится после yield
     browser.close()  # Браузер закрывается после завершения всех тестов
 
@@ -18,7 +21,7 @@ def context(browser):
     context.set_default_timeout(UIConstants.DEFAULT_UI_TIMEOUT.value)
     yield context
     log_name = f"trace_{Tools.get_timestamp()}.zip"
-    trace_path = Tools.files_dir('playwright_trace', log_name)
+    trace_path = Tools.files_dir("playwright_trace", log_name)
     context.tracing.stop(path=trace_path)
     context.close()
 
@@ -28,3 +31,27 @@ def page(context):
     page = context.new_page()
     yield page  # yield возвращает значение фикстуры, выполнение теста продолжится после yield
     page.close()  # Страница закрывается после завершения теста
+
+
+@pytest.fixture(scope="function")
+def ui_user_data() -> UIUserDemoQA:
+    return UIUserDemoQA(
+        userName=DataGenerator.generate_random_name(),
+        firstName=DataGenerator.generate_firstname(),
+        lastName=DataGenerator.generate_lastname(),
+        userEmail=DataGenerator.generate_random_email(),
+        userAge=DataGenerator.generate_age(),
+        userSalary=DataGenerator.generate_salary(),
+        userDepartament=DataGenerator.generate_departament(),
+        userPhoneNumber=DataGenerator.generate_mobile_number(),
+        password=DataGenerator.generate_random_password(),
+    )
+
+
+@pytest.fixture(scope="function")
+def ui_user_data_cinescope() -> UIUserCinescope:
+    return UIUserCinescope(
+        userName=DataGenerator.generate_random_name(),
+        userEmail=DataGenerator.generate_random_email(),
+        password=DataGenerator.generate_random_password(),
+    )
